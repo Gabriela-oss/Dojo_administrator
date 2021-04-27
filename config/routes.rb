a0efb7a277fde3be98e04679f889aaf8991b2da6
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-04-27 12:51:31 -0600

    Erased comments of active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  # get 'admin/senseis/new', to: 'senseis#new'
-  # post 'admin/senseis', to: 'senseis#create'
-
-
   resources :courses
   resources :dojos
   devise_for :judokas
