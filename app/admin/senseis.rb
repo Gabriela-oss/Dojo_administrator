d9af686986e06a5a713b441b50b92c5cd9ce82d8
Jakke Picado <jakkepicazo@gmail.com>
2021-04-27 15:36:53 -0600

    Modify senseis form in active_admin

--- app/admin/senseis.rb
+++ app/admin/senseis.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Sensei do
-  permit_params :email, :password, :password_confirmation, :name
+  permit_params :email, :password, :password_confirmation, :name, :last_name, :surname, :id_type, :nationality, :id_card, :phone_number, :photo
 
   index do
     selectable_column
@@ -19,6 +19,13 @@ ActiveAdmin.register Sensei do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :last_name
+      f.input :surname
+      f.input :id_type
+      f.input :nationality
+      f.input :id_card
+      f.input :phone_number
+      f.input :photo
       f.input :email
       f.input :password
       f.input :password_confirmation
