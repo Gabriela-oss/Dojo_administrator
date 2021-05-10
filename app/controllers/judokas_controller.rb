109edde3fb0a7752d38d2d3e09fbfbc2b55e3d2f
Jakke Picado <jakkepicazo@gmail.com>
2021-05-10 16:54:19 -0600

    We create the view fot the graphics

--- app/controllers/judokas_controller.rb
+++ app/controllers/judokas_controller.rb
@@ -6,6 +6,8 @@ class JudokasController < ApplicationController
   def show
     @judokas = Judoka.all
     @judoka = Judoka.find_by_uid!(params[:id])
+    @levels = Level.joins(:name).group('levels.name').all
+    @categories = Category.joins(:name).group('categories.name').all
   end
 
 end
